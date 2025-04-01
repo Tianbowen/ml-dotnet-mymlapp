6b496412aaeac91a46a0bffcfadc307acc1c9482
田博文 <1471758682@qq.com>
2025-04-01 11:26:55 +0800

    Add example project mymlapp
    Add project training model and sample code.

---
+++ src/Program.cs
@@ -0,0 +1,24 @@
+﻿using Mymlapp;
+
+namespace mymlapp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Add input data
+            var sampleData = new SentimentModel.ModelInput()
+            {
+                Col0 = "This restaurant was wonderful."
+            };
+
+            // Load model and predict output of sample data
+            var result = SentimentModel.Predict(sampleData);
+
+            // if Prediction is 1, sentiment is "Positive"; otherwise, sentiment is "Negative";
+            var sentiment = result.PredictedLabel == 1 ? "Positive" : "Negative";
+            Console.WriteLine($"Text: {sampleData.Col0}\nSentiment: {sentiment}");
+
+        }
+    }
+}
